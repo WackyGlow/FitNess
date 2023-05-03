f5855661d5471d2a787a0adeb63e1fa9c4fb4882
Rasmus Thyregod Kristensen <70566908+WackyGlow@users.noreply.github.com>
2023-05-03 23:48:37 +0200

    yee

---
+++ UserService/Database/UserDatabase.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Driver;
+using UserService.Models;
+
+namespace UserService.Database;
+
+public class UserDatabase
+{
+    private readonly IMongoCollection<User> _users;
+
+    public UserDatabase()
+    {
+        var connectionString = "mongodb+srv://rasm92i3:<P@n1cb0y5>@cluster0.te79kwa.mongodb.net/?retryWrites=true&w=majority";
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase("FitNess");
+        _users = database.GetCollection<User>("users");
+    }
+    
+    public List<User> GetUsers()
+    {
+        return _users.Find(user => true).ToList();
+    }
+
+    public User GetUserById(string id)
+    {
+        return _users.Find<User>(user => user.Id == id).FirstOrDefault();
+    }
+
+    public User CreateUser(User user)
+    {
+        _users.InsertOne(user);
+        return user;
+    }
+
+    public void UpdateUser(string id, User userIn)
+    {
+        _users.ReplaceOne(user => user.Id == id, userIn);
+    }
+
+    public void DeleteUser(User userIn)
+    {
+        _users.DeleteOne(user => user.Id == userIn.Id);
+    }
+    
+}
--- UserService/Models/User.cs
+++ UserService/Models/User.cs
@@ -2,5 +2,11 @@
 
 public class User
 {
-    
+    public string Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string Sex { get; set; }
+    public double Weight { get; set; }
+    public double DesiredWeight { get; set; }
 }
