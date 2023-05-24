27658d8815254b43fc2ac901392d2516473eda29
WackyGlow <70566908+WackyGlow@users.noreply.github.com>
2023-05-24 09:44:07 +0200

    fisk lever i vand

--- NutritionService/Infrastructure/MessageListener.cs
+++ NutritionService/Infrastructure/MessageListener.cs
@@ -1,6 +1,34 @@
-﻿namespace NutritionService.Infrastructure;
+﻿using Common;
+using EasyNetQ;
+using NutritionService.Controllers;
+
+namespace NutritionService.Infrastructure;
 
 public class MessageListener
 {
-    
+    private IServiceProvider _provider;
+    private string _connectionString;
+
+    public MessageListener(IServiceProvider provider, string connectionString)
+    {
+        _provider = provider;
+        _connectionString = connectionString;
+    }
+
+    public void start()
+    {
+        using (var bus = RabbitHutch.CreateBus(_connectionString))
+        {
+            bus.PubSub.Subscribe<UserDataMessage>("",HandleUserDataMessage, x => x.WithTopic(""));
+        }
+        lock (this)
+        {
+            Monitor.Wait(this);
+        }
+    }
+
+    private void HandleUserDataMessage(UserDataMessage message)
+    {
+        NutritionController
+    }
 }
