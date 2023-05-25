eb28dd227a09a02841c060a46a90f18bb3b0f6e3
Frede94 <32297979+Frede94@users.noreply.github.com>
2023-05-25 11:22:56 +0200

    app

--- FitnessUserApp/FitnessApp.Designer.cs
+++ FitnessUserApp/FitnessApp.Designer.cs
@@ -35,6 +35,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label12 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.HeightField = new System.Windows.Forms.TextBox();
@@ -50,45 +51,48 @@
             this.weightField = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
+            this.sendRequest = new System.Windows.Forms.Button();
+            this.succesLabel = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // fNameField
             // 
-            this.fNameField.Location = new System.Drawing.Point(4, 23);
+            this.fNameField.Location = new System.Drawing.Point(3, 19);
+            this.fNameField.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.fNameField.Name = "fNameField";
-            this.fNameField.Size = new System.Drawing.Size(190, 22);
+            this.fNameField.Size = new System.Drawing.Size(144, 20);
             this.fNameField.TabIndex = 1;
-            this.fNameField.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // TitleLable
             // 
             this.TitleLable.AutoSize = true;
             this.TitleLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TitleLable.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.TitleLable.Location = new System.Drawing.Point(-2, 9);
+            this.TitleLable.Location = new System.Drawing.Point(-2, 7);
+            this.TitleLable.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.TitleLable.Name = "TitleLable";
-            this.TitleLable.Size = new System.Drawing.Size(386, 51);
+            this.TitleLable.Size = new System.Drawing.Size(314, 39);
             this.TitleLable.TabIndex = 2;
             this.TitleLable.Text = "Fitness Calculator";
-            this.TitleLable.Click += new System.EventHandler(this.label1_Click);
             // 
             // lNameField
             // 
-            this.lNameField.Location = new System.Drawing.Point(204, 23);
+            this.lNameField.Location = new System.Drawing.Point(153, 19);
+            this.lNameField.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lNameField.Name = "lNameField";
-            this.lNameField.Size = new System.Drawing.Size(190, 22);
+            this.lNameField.Size = new System.Drawing.Size(144, 20);
             this.lNameField.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 16);
+            this.label1.Size = new System.Drawing.Size(67, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "First Name";
             // 
@@ -96,18 +100,20 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(204, 4);
+            this.label2.Location = new System.Drawing.Point(153, 3);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.Size = new System.Drawing.Size(67, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Last Name";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(-17, 99);
+            this.label3.Location = new System.Drawing.Point(-13, 80);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(476, 16);
+            this.label3.Size = new System.Drawing.Size(409, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "___________________________________________________________________";
             // 
@@ -116,24 +122,39 @@
             this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.panel1.Controls.Add(this.label12);
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Location = new System.Drawing.Point(417, 92);
+            this.panel1.Location = new System.Drawing.Point(313, 75);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(404, 421);
+            this.panel1.Size = new System.Drawing.Size(303, 342);
             this.panel1.TabIndex = 7;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(64, 131);
+            this.label12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(210, 24);
+            this.label12.TabIndex = 1;
+            this.label12.Text = "<<Some Results....>>";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(4, 4);
+            this.label4.Location = new System.Drawing.Point(3, 3);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(107, 29);
+            this.label4.Size = new System.Drawing.Size(84, 24);
             this.label4.TabIndex = 0;
             this.label4.Text = "Results:";
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel2.Controls.Add(this.succesLabel);
+            this.panel2.Controls.Add(this.sendRequest);
             this.panel2.Controls.Add(this.HeightField);
             this.panel2.Controls.Add(this.shreksField);
             this.panel2.Controls.Add(this.ageField);
@@ -152,40 +173,45 @@
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.lNameField);
             this.panel2.Controls.Add(this.label1);
-            this.panel2.Location = new System.Drawing.Point(7, 92);
+            this.panel2.Location = new System.Drawing.Point(5, 75);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(404, 421);
+            this.panel2.Size = new System.Drawing.Size(303, 342);
             this.panel2.TabIndex = 8;
             // 
             // HeightField
             // 
-            this.HeightField.Location = new System.Drawing.Point(4, 195);
+            this.HeightField.Location = new System.Drawing.Point(3, 158);
+            this.HeightField.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.HeightField.Name = "HeightField";
-            this.HeightField.Size = new System.Drawing.Size(190, 22);
+            this.HeightField.Size = new System.Drawing.Size(144, 20);
             this.HeightField.TabIndex = 19;
             // 
             // shreksField
             // 
-            this.shreksField.Location = new System.Drawing.Point(204, 75);
+            this.shreksField.Location = new System.Drawing.Point(153, 61);
+            this.shreksField.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.shreksField.Name = "shreksField";
-            this.shreksField.Size = new System.Drawing.Size(190, 22);
+            this.shreksField.Size = new System.Drawing.Size(144, 20);
             this.shreksField.TabIndex = 18;
             this.shreksField.Text = "Yes please!";
             // 
             // ageField
             // 
-            this.ageField.Location = new System.Drawing.Point(3, 75);
+            this.ageField.Location = new System.Drawing.Point(2, 61);
+            this.ageField.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ageField.Name = "ageField";
-            this.ageField.Size = new System.Drawing.Size(191, 22);
+            this.ageField.Size = new System.Drawing.Size(144, 20);
             this.ageField.TabIndex = 17;
             // 
             // label11
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(7, 174);
+            this.label11.Location = new System.Drawing.Point(5, 141);
+            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(52, 16);
+            this.label11.Size = new System.Drawing.Size(44, 13);
             this.label11.TabIndex = 16;
             this.label11.Text = "Height";
             // 
@@ -193,9 +219,10 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(207, 56);
+            this.label10.Location = new System.Drawing.Point(155, 46);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(33, 16);
+            this.label10.Size = new System.Drawing.Size(28, 13);
             this.label10.TabIndex = 15;
             this.label10.Text = "Sex";
             // 
@@ -203,59 +230,66 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(7, 56);
+            this.label9.Location = new System.Drawing.Point(5, 46);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(35, 16);
+            this.label9.Size = new System.Drawing.Size(29, 13);
             this.label9.TabIndex = 14;
             this.label9.Text = "Age";
             // 
             // wLossPerWeekField
             // 
-            this.wLossPerWeekField.Location = new System.Drawing.Point(207, 194);
+            this.wLossPerWeekField.Location = new System.Drawing.Point(155, 158);
+            this.wLossPerWeekField.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.wLossPerWeekField.Name = "wLossPerWeekField";
-            this.wLossPerWeekField.Size = new System.Drawing.Size(187, 22);
+            this.wLossPerWeekField.Size = new System.Drawing.Size(141, 20);
             this.wLossPerWeekField.TabIndex = 13;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(204, 174);
+            this.label8.Location = new System.Drawing.Point(153, 141);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(164, 16);
+            this.label8.Size = new System.Drawing.Size(137, 13);
             this.label8.TabIndex = 12;
             this.label8.Text = "Weight Loss Per Week";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(-36, 220);
+            this.label7.Location = new System.Drawing.Point(-27, 179);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(476, 16);
+            this.label7.Size = new System.Drawing.Size(409, 13);
             this.label7.TabIndex = 11;
             this.label7.Text = "___________________________________________________________________";
             // 
             // desWeightField
             // 
-            this.desWeightField.Location = new System.Drawing.Point(207, 144);
+            this.desWeightField.Location = new System.Drawing.Point(155, 117);
+            this.desWeightField.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.desWeightField.Name = "desWeightField";
-            this.desWeightField.Size = new System.Drawing.Size(187, 22);
+            this.desWeightField.Size = new System.Drawing.Size(141, 20);
             this.desWeightField.TabIndex = 10;
             // 
             // weightField
             // 
-            this.weightField.Location = new System.Drawing.Point(4, 144);
+            this.weightField.Location = new System.Drawing.Point(3, 117);
+            this.weightField.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.weightField.Name = "weightField";
-            this.weightField.Size = new System.Drawing.Size(190, 22);
+            this.weightField.Size = new System.Drawing.Size(144, 20);
             this.weightField.TabIndex = 9;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(204, 124);
+            this.label6.Location = new System.Drawing.Point(153, 101);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(114, 16);
+            this.label6.Size = new System.Drawing.Size(94, 13);
             this.label6.TabIndex = 8;
             this.label6.Text = "Desired Weight";
             // 
@@ -263,32 +297,44 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(7, 124);
+            this.label5.Location = new System.Drawing.Point(5, 101);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(55, 16);
+            this.label5.Size = new System.Drawing.Size(47, 13);
             this.label5.TabIndex = 7;
             this.label5.Text = "Weight";
             // 
-            // label12
+            // sendRequest
             // 
-            this.label12.AutoSize = true;
-            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(86, 161);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(263, 29);
-            this.label12.TabIndex = 1;
-            this.label12.Text = "<<Some Results....>>";
+            this.sendRequest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sendRequest.Location = new System.Drawing.Point(4, 308);
+            this.sendRequest.Name = "sendRequest";
+            this.sendRequest.Size = new System.Drawing.Size(75, 28);
+            this.sendRequest.TabIndex = 20;
+            this.sendRequest.Text = "Send";
+            this.sendRequest.UseVisualStyleBackColor = true;
+            this.sendRequest.Click += new System.EventHandler(this.sendRequest_Click);
+            // 
+            // succesLabel
+            // 
+            this.succesLabel.AutoSize = true;
+            this.succesLabel.Location = new System.Drawing.Point(86, 322);
+            this.succesLabel.Name = "succesLabel";
+            this.succesLabel.Size = new System.Drawing.Size(16, 13);
+            this.succesLabel.TabIndex = 21;
+            this.succesLabel.Text = "...";
             // 
             // FitnessApp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(828, 521);
+            this.ClientSize = new System.Drawing.Size(621, 423);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.TitleLable);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FitnessApp";
             this.Text = "Fitness Calculator";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -325,6 +371,8 @@
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button sendRequest;
+        private System.Windows.Forms.Label succesLabel;
     }
 }
 
--- FitnessUserApp/FitnessApp.cs
+++ FitnessUserApp/FitnessApp.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FitnessUserApp
 {
     public partial class FitnessApp : Form
     {
+        HttpClient client = new HttpClient();
+        string apiUrl = "https://localhost:7184/user";
+
         public FitnessApp()
         {
             InitializeComponent();
@@ -22,14 +22,71 @@ namespace FitnessUserApp
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private async void sendRequest_Click(object sender, EventArgs e)
         {
+            string _firstName = fNameField.Text;
+            string _lastName = fNameField.Text;
+            int _age = int.Parse(ageField.Text);
+            string _sex = shreksField.Text;
+            double _weight = double.Parse(weightField.Text);
+            double _desiredWeight = double.Parse(desWeightField.Text);
+            double _height = double.Parse(HeightField.Text);
+            double _lossPrWeek = double.Parse(wLossPerWeekField.Text);
 
-        }
+            var data = new
+            {
+                FirstName = _firstName,
+                LastName = _lastName,
+                Age = _age,
+                Sex = _sex,
+                Weight = _weight,
+                DesiredWeight = _desiredWeight,
+                Height = _height,
+                LossPrWeek = _lossPrWeek                
+            };
 
-        private void label1_Click(object sender, EventArgs e)
-        {
+            //Serialize the object to JSON format using Newtonsoft.Json
+            string jsonBody = JsonConvert.SerializeObject(data);
 
+            //Create a new instance of the StringContent class with the JSON body and
+            //specify the content type as "application/json"
+            var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+            try
+            {
+                //Use the HttpClient instance to call the API endpoint within your solution
+                //by specifying the appropriate HTTP method (e.g., POST, PUT, GET, etc.) and passing the content
+                HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // The request was successful, handle the response
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    // Parse the response body if necessary
+
+                    HttpStatusCode statusCode = response.StatusCode;
+                    succesLabel.Text = statusCode.ToString();
+                }
+                else
+                {
+                    // The request failed, handle the error
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    // You can access the status code, reason phrase, and response body from the response object
+                    HttpStatusCode statusCode = response.StatusCode;
+                    string reasonPhrase = response.ReasonPhrase;
+
+                    succesLabel.Text = "!Some Error Happened!";
+
+                    // Handle the error based on the status code or response content
+                    // For example, you can throw a custom exception or display an error message to the user
+                    throw new Exception($"API request failed with status code: {statusCode}, reason: {reasonPhrase}, response: {responseBody}");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that occurred during the API call
+                // You can log the exception or display an error message to the user
+                Console.WriteLine($"An error occurred while calling the API: {ex.Message}");
+            }
         }
     }
 }
