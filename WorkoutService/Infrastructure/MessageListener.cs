1a245b6189bdf43bcbe6bb1020a307b3deed4514
Rasmus Thyregod Kristensen <70566908+WackyGlow@users.noreply.github.com>
2023-05-26 01:23:07 +0200

    added messagelistener to Workout

---
+++ WorkoutService/Infrastructure/MessageListener.cs
@@ -0,0 +1,45 @@
+﻿using Common;
+using EasyNetQ;
+using WorkoutService.Database;
+using WorkoutService.Models;
+
+namespace WorkoutService.Infrastructure;
+
+public class MessageListener
+{
+    private IServiceProvider _provider;
+    private string _connectionString;
+    private WorkoutCollection _workoutCollection;
+
+    public MessageListener(IServiceProvider provider, string connectionString)
+    {
+        _provider = provider;
+        _connectionString = connectionString;
+        _workoutCollection = new WorkoutCollection();
+    }
+
+    public void start()
+    {
+        using (var bus = RabbitHutch.CreateBus(_connectionString))
+        {
+            //bus.PubSub.Subscribe<UserDataMessage>("nutritionAPI",HandleUserDataMessage, x => x.WithTopic("userCalorieIntake"));
+            bus.PubSub.Subscribe<SetWorkoutIntensityMessage>("workoutAPI", HandleSetWorkoutHandler,
+                x => x.WithTopic("setUserIntensity"));
+        }
+        lock (this)
+        {
+            Monitor.Wait(this);
+        }
+    }
+
+    private void HandleSetWorkoutHandler(SetWorkoutIntensityMessage obj)
+    {
+        var intensityToAdd = new WorkoutIntensity
+        {
+            UserId = obj.UserId,
+            WorkoutId = new Guid(),
+            WorkOutIntensity = obj.WorkOutIntensity
+        };
+        _workoutCollection.SetWorkOutIntensity(intensityToAdd);
+    }
+}
