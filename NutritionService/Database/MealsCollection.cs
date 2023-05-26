453bf1e6aa02c06a6c29a206885c52efb486a2f5
Frede94 <32297979+Frede94@users.noreply.github.com>
2023-05-26 12:54:57 +0200

    Added Documentation

--- NutritionService/Database/MealsCollection.cs
+++ NutritionService/Database/MealsCollection.cs
@@ -3,10 +3,17 @@ using NutritionService.Models;
 
 namespace NutritionService.Database;
 
+
+/// <summary>
+/// Represents a collection for storing meals in a MongoDB database.
+/// </summary>
 public class MealsCollection
 {
     private readonly IMongoCollection<MealList> _meals;
 
+    /// <summary>
+    /// Initializes a new instance of the MealsCollection class.
+    /// </summary>
     public MealsCollection()
     {
         // Connect to the MongoDB database
@@ -17,7 +24,12 @@ public class MealsCollection
         // Initialize the collection for users
         _meals = database.GetCollection<MealList>("MealsCollection");
     }
-    
+
+    /// <summary>
+    /// Adds a meals list to the collection.
+    /// </summary>
+    /// <param name="meals">The meals list to be added.</param>
+    /// <returns>The added meals list.</returns>
     public MealList AddMealsList(MealList meals)
     {
         _meals.InsertOne(meals);
--- NutritionService/Infrastructure/MessageListener.cs
+++ NutritionService/Infrastructure/MessageListener.cs
@@ -4,12 +4,20 @@ using NutritionService.Controllers;
 
 namespace NutritionService.Infrastructure;
 
+/// <summary>
+/// Represents a message listener for receiving user data messages.
+/// </summary>
 public class MessageListener
 {
     private IServiceProvider _provider;
     private string _connectionString;
     private IBus _bus;
 
+    /// <summary>
+    /// Initializes a new instance of the MessageListener class with the specified service provider and RabbitMQ connection string.
+    /// </summary>
+    /// <param name="provider">The service provider used for dependency injection.</param>
+    /// <param name="connectionString">The RabbitMQ connection string.</param>
     public MessageListener(IServiceProvider provider, string connectionString)
     {
         _provider = provider;
@@ -17,6 +25,9 @@ public class MessageListener
         _bus = RabbitHutch.CreateBus(_connectionString);
     }
 
+    /// <summary>
+    /// Starts listening for user data messages.
+    /// </summary>
     public void start()
     {
         _bus.PubSub.Subscribe<UserDataMessage>("nutritionApiTGUserCalorieIntake",HandleUserDataMessage, x => x.WithTopic("userCalorieIntake"));
@@ -29,6 +40,9 @@ public class MessageListener
         Console.WriteLine("Received message: ID: " + message.Id + " | Weight: " + message.Weight + " | Height: " + message.Height + " | Age :" + message.Age + " | Sex: " + message.Sex + " | KgLossPerWeek: " + message.KgLossPerWeek);
     }
 
+    /// <summary>
+    /// Releases the resources used by the MessageListener object.
+    /// </summary>
     public void Dispose()
     {
         // Dispose the bus and close the RabbitMQ connection
